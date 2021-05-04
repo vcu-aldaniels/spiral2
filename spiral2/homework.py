a4c067385e2a896da6bee1a2445d8fb0c36bc006
Alexis Daniels <danielsam3@vcu.edu>
2021-05-03 23:43:58 -0400

    change 3

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -1,14 +1,15 @@
 def spiralize(size, n=1):
     """ Your code goes somewhere in here"""
-    size = 101 #this the sum of numbers size from diagonal
-    n = 25 #this is what the number is in place of 1 essentially its 25 counting backwards to one instead of from 1
+    size = 101  # this the sum of numbers size from diagonal
+    n = 25  # this is what the number is in place of 1 essentially its 25 counting backwards to one instead of from 1
 
-    for x in range (25, n-1):
+    for x in range(25, n - 1):
         if n == 25:
             return n
 
     return_value = n
     return return_value
+
 
 if __name__ == "__main__":
     find_sum = spiralize(501)
