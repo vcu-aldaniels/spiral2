1ab32d09e7afc2475d561cb2a0d7091e3bcb48de
Alexis Daniels <danielsam3@vcu.edu>
2021-05-03 23:54:44 -0400

    cha ge 6

--- spiral2/homework.py
+++ spiral2/homework.py
@@ -3,7 +3,9 @@ def spiralize(size, n=1):
     size = 101  # this the sum of numbers size from diagonal
     n = 25  # this is what the number is in place of 1 essentially its 25 counting backwards to one instead of from 1
 
-    for x in range(25, n - 1): #this is here so that every is counting down from 25 instead of counting up from 1.
+    for x in range(
+        25, n - 1
+    ):  # this is here so that every is counting down from 25 instead of counting up from 1.
         if n == 25:
             return n
 
